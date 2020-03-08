b72ebf490890679a7699560a777a196556ed4313
NBKelly <nbk4@students.waikato.ac.nz>
2020-03-09 11:53:13 +1300

    Helper class that represents an individual node

---
+++ Node.java
@@ -0,0 +1,30 @@
+public class Node {
+    int id;
+    int target;
+
+    public Node(int id, int target) {
+	this.id = id;
+	this.target = target;
+    }
+
+    public int getID() {
+	return id;
+    }
+
+    public int getTarget() {
+	return target;
+    }
+
+    public String toString() {
+	return String.format("Node %d -> %d", id, target);
+    }
+
+    public String toChain(boolean c) {
+	String res = "" + id;
+
+	if(c)
+	    res += " -> ";
+
+	return res;
+    }
+}
