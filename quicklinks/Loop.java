e012643148972c42996ccd3c065786e1ad53d0e9
NBKelly <nbk4@students.waikato.ac.nz>
2020-03-11 00:41:22 +1300

    corrected dist function

--- quicklinks/Loop.java
+++ quicklinks/Loop.java
@@ -1,5 +1,6 @@
 package quicklinks;
 
+import java.util.TreeSet;
 import java.util.TreeMap;
 import java.util.List;
 
@@ -7,8 +8,13 @@ public class Loop {
     //what we want is a set of nodes in the format: node -> index, which can be accessed from just a node
     //given any two nodes, we should be able to figure out the distance between them at O(1)
     TreeMap<Node, Integer> nodes;
-    int size = 0;
 
+    //given any ancestor, we should be able to figure out WHERE on the ring it attaches
+    TreeMap<Node, Integer> ancestors;
+    
+    int size = 0;
+    int ancestorCount = 0;
+    
     public Loop(List<Node> nodeList, boolean debug) {
 	nodes = new TreeMap<>();
 	int index = 0;
@@ -38,13 +44,37 @@ public class Loop {
 	return nodes.containsKey(n);
     }
 
+    public void associateAncestors(List<TreeSet<Node>> ancestorList, boolean DEBUG) {
+	ancestors = new TreeMap<Node, Integer>();
+
+	for(Node n : nodes.keySet()) {
+	    //this is the index in the ring
+	    int index = nodes.get(n);
+	    int id = n.getID();
+	    
+	    //now we can find all the ancestors of our node
+	    TreeSet<Node> idAncestors = ancestorList.get(id);
+	    for(Node a : idAncestors) {
+		//don't add things already in our loop as ancestors
+		if(nodes.containsKey(a))
+		    continue;
+
+		ancestors.put(a, index);
+		if(DEBUG)
+		    System.err.printf("NODE %d ANCESTOR OF LOOP AT INDEX %d%n", a.getID(), index);
+	    }
+	}
+    }
+    
     public int dist(Node s, Node d) {
 	int start = nodes.get(s);
 	int end = nodes.get(d);
 	int dist = end - start;
 
+	System.err.printf("%d, %d, %d: ", start, end, dist);
+	
 	if(dist < 0)
-	    dist = size - dist;
+	    dist = size + dist;
 
 	return dist;
     }
