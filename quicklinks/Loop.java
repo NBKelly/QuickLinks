467786c23945bde2f5523874546e2f089a6a4f9c
NBKelly <nbk4@students.waikato.ac.nz>
2020-03-12 21:13:58 +1300

    updated loop to be O(1) instead of O(log(n))

--- quicklinks/Loop.java
+++ quicklinks/Loop.java
@@ -1,87 +1,34 @@
 package quicklinks;
 
-import java.util.TreeSet;
-import java.util.TreeMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Loop {
-    //what we want is a set of nodes in the format: node -> index, which can be accessed from just a node
-    //given any two nodes, we should be able to figure out the distance between them at O(1)
-    TreeMap<Node, Integer> nodes;
+    //we don't actually need to keep a list of nodes - only a list of ancestors
+    private TreeSet<Node> ancestors = new TreeSet<>();
 
-    //given any ancestor, we should be able to figure out WHERE on the ring it attaches
-    TreeMap<Node, Integer> ancestors;
+    private int size;
     
-    int size = 0;
-    int ancestorCount = 0;
-
-    public Set<Node> getAncestors() {
-	return ancestors.keySet();
-    }
-    
-    public Loop(List<Node> nodeList, boolean debug) {
-	nodes = new TreeMap<>();
-	int index = 0;
-
-	StringBuilder sb = new StringBuilder();
-
-	
-	if(debug)
-	    System.err.println("Loop starting at node " + nodeList.get(0).getID());
-	
-	for(Node n : nodeList) {
-	    //not sure if we need to start at 0 or 1
-	    if(debug)
-		System.err.printf("%d (%d) ---> ", n.getID(), index + 1);
-	    nodes.put(n, ++index);
-		
-	}
-
-	if(debug)
-	    System.err.println();
-	
-	size = nodes.size();
-	
+    public Loop(int size) {
+	this.size = size;
     }
 
-    public boolean contains(Node n) {
-	return nodes.containsKey(n);
-    }
-
-    public void associateAncestors(List<TreeSet<Node>> ancestorList, boolean DEBUG) {
-	ancestors = new TreeMap<Node, Integer>();
-
-	for(Node n : nodes.keySet()) {
-	    //this is the index in the ring
-	    int index = nodes.get(n);
-	    int id = n.getID();
-	    
-	    //now we can find all the ancestors of our node
-	    TreeSet<Node> idAncestors = ancestorList.get(id);
-	    for(Node a : idAncestors) {
-		//don't add things already in our loop as ancestors
-		if(nodes.containsKey(a))
-		    continue;
-
-		ancestors.put(a, index);
-		if(DEBUG)
-		    System.err.printf("NODE %d ANCESTOR OF LOOP AT INDEX %d%n", a.getID(), index);
-	    }
-	}
-    }
-    
-    public int dist(Node s, Node d) {
-	if(s == d)
-	    return 0;
-	
-	int start = nodes.get(s);
-	int end = nodes.get(d);
+    public int dist(int start, int end) {	
 	int dist = end - start;
-
 	if(dist < 0)
 	    dist = size + dist;
 
 	return dist;
+    }
+
+    public void associateAncestors(TreeSet<Node> idAncestors) {
+	ancestors.addAll(idAncestors);
+    }
+
+    public Set<Node> getAncestors() {
+	return ancestors;
+    }
+
+    public int size() {
+	return size;
     }
 }
