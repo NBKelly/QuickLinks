cd9b20d85799bbb098d539d5842082b47736664f
NBKelly <nbk4@students.waikato.ac.nz>
2020-03-14 14:13:49 +1300

    updated to take measures of pre-processing time and post processing time (calculated) for caomparison

--- metric.sh
+++ metric.sh
@@ -18,7 +18,21 @@ do
     cat $FILE | java quicklinks.QuickLinks > tmp_out.out
     end=`date +%s.%N`
     runtime=$(python -c "print((${end} - ${start})/5.0)")
-    echo $runtime
+    echo "Total: " $runtime
+
+    start=`date +%s.%N`
+    cat $FILE | java quicklinks.QuickLinks -bt > trash.out
+    cat $FILE | java quicklinks.QuickLinks -bt > trash.out
+    cat $FILE | java quicklinks.QuickLinks -bt > trash.out
+    cat $FILE | java quicklinks.QuickLinks -bt > trash.out
+    cat $FILE | java quicklinks.QuickLinks -bt > trash.out
+    end=`date +%s.%N`
+    runtime2=$(python -c "print((${end} - ${start})/5.0)")
+    echo "Pre:   " $runtime2
+
+    runtime3=$(python -c "print((${runtime} - ${runtime2}))")
+    #display the difference in runtimes    
+    echo "Post:  " $runtime3
 
     #now we want to see if the answer is valid
     filename=$(basename -- "$FILE")
@@ -28,5 +42,7 @@ do
     diff $filename tmp_out.out > differences
     wc -l differences
     rm differences
+
+    
     echo
 done
